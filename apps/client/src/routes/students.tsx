8fdc9732878b2ee93dbe23990d84d2ba0f807703
fluwdev <jesusalcalarojas@gmail.com>
2025-06-04 20:36:25 -0400

    feat: remove students route component

--- apps/client/src/routes/students.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/students')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/students"!</div>
-}
